07952f33626ec098b274588f5afcbd300c63d5e2
Allison Drake <junesol.drake@gmail.com>
2023-10-18 20:20:50 +0000

    finishedgit add wikipedia-game-sc.py!

--- wikipedia-game-sc.py
+++ wikipedia-game-sc.py
@@ -26,11 +26,32 @@ def wikipedia_game_solver(start_page, target_page):
     visited = []
     queue = Queue()
     path = []
+    parent = {}
 
+    queue.put(start_page.title)
     visited.append(start_page.title)
     
     while not queue.empty():
-        
+        current_title = queue.get()
+        if current_title == target_page.title:
+            break
+
+        current_page = wiki_wiki.page(current_title)
+        next_level = fetch_links(current_page)
+        visited.append(current_title)
+
+        for node in next_level:
+            if node not in visited:
+                queue.put(node)
+                parent[node] = current_title
+    
+    child = target_page.title
+    while child != start_page.title:
+        path.append(child)
+        child = parent[child]
+    path.append(start_page.title)
+    path.reverse()
+
     #queue.get() gets the first thing in the queue
     #queue.put() puts something at the end of the queue
 
@@ -41,8 +62,8 @@ def wikipedia_game_solver(start_page, target_page):
     return path
 
 # Example usage:
-start_page = wiki_wiki.page('Nina Tandon')
-target_page = wiki_wiki.page('Italian language')
+start_page = wiki_wiki.page('Pasadena High School')
+target_page = wiki_wiki.page('Rose Bowl')
 path = wikipedia_game_solver(start_page, target_page)
 print("Shortest path:", path)
 
